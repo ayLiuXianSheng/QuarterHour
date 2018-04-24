7dadf7e7b73a3c4035685a753d4beae2ad57239d
农夫三拳 <1227522057@qq.com>
2018-04-24 14:29:41 +0800

    一刻钟

--- app/src/main/java/com/example/lenovo/quarterhour/MainActivity.java
+++ app/src/main/java/com/example/lenovo/quarterhour/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.lenovo.quarterhour;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//分支
 public class MainActivity extends AppCompatActivity {
 
     @Override
